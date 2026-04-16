28b3ee39a2b23c48e3452bf39adac7397395f55d
Emanuele De Rocchi <leledero01@gmail.com>
2026-04-16 23:55:33 +0200

    aggiornamento portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import HomeEN from "./HomeEN";
 import HomeIT from "./HomeIT";
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <>
       <SpeedInsights />
+      <Analytics />
 
       <Routes>
         <Route path="/" element={<HomeEN />} />
